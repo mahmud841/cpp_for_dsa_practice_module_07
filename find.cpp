e23a946ecaa88d8ff4f50c25a00343807746350b
mahmud841 <mahmudsuruj14@gmail.com>
2024-09-17 15:48:03 +0800

    find module added

---
+++ find.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "Hello World Hello World";
+    // // int t =s.find("World");
+    // // int t = s.find("Word");
+    // // cout << t << endl;
+    // while (s.find("World") != -1)
+    // {
+    //     s.replace(s.find("World"), 5, "$");
+    // }
+    // cout << s << endl;
+    string s = "BRITISHEGYPTGHANA";
+    int t = s.find("EGYPT");
+    cout << t << endl;
+    return 0;
+}
